aed46c04a588a3804e04abb5f0f69ed71d6f4241
Tanya Valdez <TValdez@digmap.com>
2018-09-04 19:01:21 -0700

    add array to app.js and cdn to index.html

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-var x = 0
+var books = [
+  "Pride and Prejudice", "Odyssey", "The Great Gatsby", "Hamlet", "Da Vinci Code", "Life of Pi", "The Help", "To Kill a Mockingird", "The Lord of the Rings", "The Handmaid's Tale"
+
+];
